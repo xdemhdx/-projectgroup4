09692e67ecbc2207fb97647a842c04f74c22312a
Ayoub Feki <60102592@udst.edu.qa>
2023-11-07 14:09:22 +0300

    Adding testing

---
+++ testing_files/unit_tests.py
@@ -0,0 +1,15 @@
+import unittest
+#import datalayer.menu_management # you have to change this according to your folder/file name
+
+
+class UnitTestsMenuManagement(unittest.TestCase):
+    def test_import_json(self):
+        recipe_result=menu_management.Test.import_file()# you have to change this according to your import function
+        self.assertTrue(recipe_result[0])
+        self.assertTrue(len(recipe_result[1])==1)
+        self.assertTrue(recipe_result[1][0]["recipes"][0]["ingredients"][0]["amount"]>0)
+        self.assertTrue(recipe_result[1][0]["recipes"][0]["preparation time (minutes)"]>0)
+        self.assertTrue(recipe_result[1][0]["recipes"][0]["cooking time (minutes)"]>0)
+        self.assertTrue(recipe_result[1][0]["recipes"][0]["servings"]>0)
+        self.assertTrue(recipe_result[1][0]["recipes"][0]["calories"]>0)
+        self.assertTrue(recipe_result[1][0]["recipes"][0]["id"]>0)
