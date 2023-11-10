e7b5d08725083dd1bf4c183d0baa4a000a5c0f27
Omar Abdulaziz <60100071@udst.edu.qa>
2023-11-10 20:12:32 +0300

    created half of the project

--- app.py
+++ app.py
@@ -1,33 +1,104 @@
-
+from http import HTTPStatus
 from flask import flash, request
 import sys
+from extensions import db
 from flask import Flask, jsonify, redirect, render_template, url_for
 from flask_migrate import Migrate
 from flask_restful import Api
+from models.Recipe import Recipe
+from config import Config
 
 
 def create_app():
     print("Hello", file=sys.stderr)
     app = Flask(__name__)
-    #app.config.from_object(Config)
-    # register_extensions(app)
+    app.config.from_object(Config)
+    register_extensions(app)
     # register_resources(app)
     routes(app)
 
     return app
 
 
-# def register_extensions(app):
-#     db.init_app(app)
-#     migrate = Migrate(app, db)
+def register_extensions(app):
+    db.init_app(app)
+    migrate = Migrate(app, db)
 
 
-def register_resources(app):
-    api = Api(app)
+# def register_resources(app):
+#     api = Api(app)
 def routes(app):
     @app.route('/')
     def home():
-        return render_template("index.html")
+        # the below code is to insert the data uncomeent it then refresh the page as many times as you want so
+        # the data is inserted then comment it back 
+        # new_recipe_data = {
+        # 'recipe_name': 'Chocolate Cake',
+        # 'instructions': 'Mix ingredients and bake at 350 degrees for 35 minutes.',
+        # 'preparation_time_minutes': 20,
+        # 'cooking_time_minutes': 35,
+        # 'servings': 8,
+        # 'calories': 300,
+        # 'category': 'Dessert',
+        # 'ingredients': [
+        #     {'name': 'Flour', 'amount': '2 cups'},
+        #     {'name': 'Sugar', 'amount': '1 cup'},
+        #     {'name': 'Cocoa Powder', 'amount': '1/2 cup'}]
+        # }
+        # new_recipe = Recipe(
+        #     recipe_name=new_recipe_data['recipe_name'],
+        #     instructions=new_recipe_data['instructions'],
+        #     preparation_time_minutes=new_recipe_data['preparation_time_minutes'],
+        #     cooking_time_minutes=new_recipe_data['cooking_time_minutes'],
+        #     servings=new_recipe_data['servings'],
+        #     calories=new_recipe_data['calories'],
+        #     category=new_recipe_data['category'],
+        #     ingredients=new_recipe_data['ingredients']  # Assuming ingredients is a JSON-compatible list
+        #     )
+        # new_recipe.save()
+        return render_template('index.html')
+    # guys this route will redirect the user to a page where it will show all the recipes in the database
+    @app.route('/recipes')
+    def all():
+        all_recipes = Recipe.get_all()
+        return render_template('allrecipes.html', recipes=all_recipes)
+    # guys this route will return a recipe by id 
+    @app.route('/search')
+    def get_by_id():
+        rid = request.args.get('search')
+        if rid.isdigit():
+            rid = int(rid)
+            data = Recipe.get_by_id(rid)
+            if data is None:
+                return  {"Message":" recipe not found"}
+            return render_template('search-recipe.html',recipes=data)
+        return {"Message":"Entry type shoulb be integer or number"}
+        # if type(rid) != int:
+        #     return {"Message":"Error Id should be type of integer"}
+        # return rid
+    @app.route('/update-recipe',methods=["POST"])
+    def update_recipe():
+        recipe_id = request.form.get('id')
+        recipe_name = request.form.get('recipe_name')
+        instructions = request.form.get('instructions')
+        data = {
+        'recipe_name': recipe_name,
+        'instructions': instructions
+        }
+        response, status = Recipe.update(recipe_id, data)
+        if status == HTTPStatus.OK:
+        # If update is successful
+            return jsonify(response), status
+        else:
+        # If recipe is not found or any other error
+            return jsonify({'message': 'Update failed'}), status
+
+    @app.route("/<int:recipe_id>/delete")
+    def delete_recipe(recipe_id):
+        respones , status = Recipe.delete(recipe_id)
+        return respones
+
+
 
 
 if __name__ == '__main__':
--- models/Recipe.py
+++ models/Recipe.py
@@ -2,23 +2,21 @@ from extensions import db
 from http import HTTPStatus
 class Recipe(db.Model):
     __tablename__ = 'recipe'
-
     id = db.Column(db.Integer, primary_key=True)
     recipe_name = db.Column(db.String(100), nullable=False)
-    ingredients = db.relationship('Ingredient', backref='recipe', lazy=True)
     instructions = db.Column(db.String(1000))  # Adjust size as needed
     preparation_time_minutes = db.Column(db.Integer)
     cooking_time_minutes = db.Column(db.Integer)
     servings = db.Column(db.Integer)
     calories = db.Column(db.Integer)
     category = db.Column(db.String(50))
-
+    ingredients = db.Column(db.JSON)  # Storing ingredients as a JSON field
     @property
     def data(self):
         return {
             'id': self.id,
             'recipe_name': self.recipe_name,
-            'ingredients': [ingredient.to_dict() for ingredient in self.ingredients],
+            'ingredients': self.ingredients,  # Directly return the JSON field
             'instructions': self.instructions,
             'preparation_time_minutes': self.preparation_time_minutes,
             'cooking_time_minutes': self.cooking_time_minutes,
@@ -35,7 +33,11 @@ class Recipe(db.Model):
         return [recipe.data for recipe in result]
     @classmethod
     def get_by_id(cls,id):
-        return cls.query.filter(cls.id==id).first()
+        result = cls.query.filter(cls.id == id).first()
+        return result.data if result else None
+            
+
+        #return cls.query.filter(cls.id==id).first() later will use it when implementing the controller to fetch the data
     @classmethod
     def update(cls, id, data):
         recipe = cls.query.filter_by(id=id).first()
@@ -43,11 +45,6 @@ class Recipe(db.Model):
             return {'message': 'Recipe not found'}, HTTPStatus.NOT_FOUND
         recipe.recipe_name = data.get('recipe_name', recipe.recipe_name)
         recipe.instructions = data.get('instructions', recipe.instructions)
-        recipe.preparation_time_minutes = data.get('preparation_time_minutes', recipe.preparation_time_minutes)
-        recipe.cooking_time_minutes = data.get('cooking_time_minutes', recipe.cooking_time_minutes)
-        recipe.servings = data.get('servings', recipe.servings)
-        recipe.calories = data.get('calories', recipe.calories)
-        recipe.category = data.get('category', recipe.category)
         db.session.commit()
         return recipe.data, HTTPStatus.OK
     @classmethod
@@ -61,29 +58,3 @@ class Recipe(db.Model):
 
     
         
-
-
-class Ingredient(db.Model):
-    __tablename__ = 'ingredient'
-
-    id = db.Column(db.Integer, primary_key=True)
-    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
-    ingredient = db.Column(db.String(50), nullable=False)
-    amount = db.Column(db.Integer, nullable=False)
-    @classmethod
-    def to_dict(self):
-        return {
-            'ingredient': self.ingredient,
-            'amount': self.amount
-        } 
-# @classmethod
-# def post(cls, recipe_id, ingredient_data):
-#         new_ingredient = cls(
-#             recipe_id=recipe_id,
-#             ingredient=ingredient_data['ingredient'],
-#             amount=ingredient_data['amount']
-#         )
-#         db.session.add(new_ingredient)
-#         db.session.commit()
-
-#         return new_ingredient.to_dict(), HTTPStatus.CREATED
