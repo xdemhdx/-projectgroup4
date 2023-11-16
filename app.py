b62394de060fae5861373490218965da517a8f57
Omar abdulaziz <60100071@udst.edu.qa>
2023-11-16 10:25:38 +0300

    Added many things

--- app.py
+++ app.py
@@ -58,14 +58,41 @@ def routes(app):
         # new_recipe.save()
         return render_template('index.html')
     # guys this route will redirect the user to a page where it will show all the recipes in the database
+    @app.route('/add')
+    def add():
+        return render_template("addmenu.html")
+    @app.route("/add-recipe",methods=["POST"])
+    def addrecipe():
+        recipe_name = request.form.get('recipe_name')
+        instructions = request.form.get('instructions')
+        preparation_time = request.form.get('preparation_time')
+        cooking_time = request.form.get('cooking_time')
+        servings = request.form.get('servings')
+        calories = request.form.get('calories')
+        category = request.form.get('category')
+        data = {
+            "recipe_name":recipe_name,
+            "instructions": instructions,
+            "preparation_time_minutes":preparation_time,
+            "cooking_time_minutes": cooking_time,
+            "servings": servings,
+            "calories":calories,
+            "category":category,
+            "ingredients":{"name":"flour","amount":"5 cups"}
+        }
+        result = Recipe(recipe_name=data['recipe_name'],instructions=data['instructions'],preparation_time_minutes=data['preparation_time_minutes'],cooking_time_minutes=data['cooking_time_minutes'],servings=data['servings'],calories=data['calories'],category=data['category'],ingredients=data['ingredients'])
+        result.save()
+        return result.data
+
     @app.route('/recipes')
     def all():
         all_recipes = Recipe.get_all()
         return render_template('allrecipes.html', recipes=all_recipes)
     # guys this route will return a recipe by id 
-    @app.route('/search')
+    @app.route('/search',methods=["GET"])
     def get_by_id():
-        rid = request.args.get('search')
+        #return {"Message": "Debugging"}
+        rid = request.args.get('id')
         if rid.isdigit():
             rid = int(rid)
             data = Recipe.get_by_id(rid)
@@ -73,9 +100,9 @@ def routes(app):
                 return  {"Message":" recipe not found"}
             return render_template('search-recipe.html',recipes=data)
         return {"Message":"Entry type shoulb be integer or number"}
-        # if type(rid) != int:
-        #     return {"Message":"Error Id should be type of integer"}
-        # return rid
+        if type(rid) != int:
+            return {"Message":"Error Id should be type of integer"}
+        return rid
     @app.route('/update-recipe',methods=["POST"])
     def update_recipe():
         recipe_id = request.form.get('id')
@@ -86,9 +113,10 @@ def routes(app):
         'instructions': instructions
         }
         response, status = Recipe.update(recipe_id, data)
+        data = Recipe.get_by_id(recipe_id)
         if status == HTTPStatus.OK:
         # If update is successful
-            return jsonify(response), status
+            return render_template('search-recipe.html',recipes=data)
         else:
         # If recipe is not found or any other error
             return jsonify({'message': 'Update failed'}), status
@@ -96,7 +124,7 @@ def routes(app):
     @app.route("/<int:recipe_id>/delete")
     def delete_recipe(recipe_id):
         respones , status = Recipe.delete(recipe_id)
-        return respones
+        return {"Response":respones,"Status":status}
 
 
 
