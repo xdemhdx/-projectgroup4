3d055c11ef2b8752356dbdd183616eaa4d46f5f0
Omar Abdulaziz <60100071@udst.edu.qa>
2023-11-16 16:07:55 +0300

    cleaned up app.py

--- app.py
+++ app.py
@@ -14,48 +14,16 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     register_extensions(app)
-    # register_resources(app)
     routes(app)
-
     return app
-
 
 def register_extensions(app):
     db.init_app(app)
     migrate = Migrate(app, db)
 
-
-# def register_resources(app):
-#     api = Api(app)
 def routes(app):
     @app.route('/')
     def home():
-        # the below code is to insert the data uncomeent it then refresh the page as many times as you want so
-        # the data is inserted then comment it back 
-        # new_recipe_data = {
-        # 'recipe_name': 'Chocolate Cake',
-        # 'instructions': 'Mix ingredients and bake at 350 degrees for 35 minutes.',
-        # 'preparation_time_minutes': 20,
-        # 'cooking_time_minutes': 35,
-        # 'servings': 8,
-        # 'calories': 300,
-        # 'category': 'Dessert',
-        # 'ingredients': [
-        #     {'name': 'Flour', 'amount': '2 cups'},
-        #     {'name': 'Sugar', 'amount': '1 cup'},
-        #     {'name': 'Cocoa Powder', 'amount': '1/2 cup'}]
-        # }
-        # new_recipe = Recipe(
-        #     recipe_name=new_recipe_data['recipe_name'],
-        #     instructions=new_recipe_data['instructions'],
-        #     preparation_time_minutes=new_recipe_data['preparation_time_minutes'],
-        #     cooking_time_minutes=new_recipe_data['cooking_time_minutes'],
-        #     servings=new_recipe_data['servings'],
-        #     calories=new_recipe_data['calories'],
-        #     category=new_recipe_data['category'],
-        #     ingredients=new_recipe_data['ingredients']  # Assuming ingredients is a JSON-compatible list
-        #     )
-        # new_recipe.save()
         return render_template('index.html')
     # guys this route will redirect the user to a page where it will show all the recipes in the database
     @app.route('/add')
@@ -91,7 +59,6 @@ def routes(app):
     # guys this route will return a recipe by id 
     @app.route('/search',methods=["GET"])
     def get_by_id():
-        #return {"Message": "Debugging"}
         rid = request.args.get('id')
         if rid.isdigit():
             rid = int(rid)
@@ -100,9 +67,6 @@ def routes(app):
                 return  {"Message":" recipe not found"}
             return render_template('search-recipe.html',recipes=data)
         return {"Message":"Entry type shoulb be integer or number"}
-        if type(rid) != int:
-            return {"Message":"Error Id should be type of integer"}
-        return rid
     @app.route('/update-recipe',methods=["POST"])
     def update_recipe():
         recipe_id = request.form.get('id')
